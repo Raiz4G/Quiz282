1d5d9d059ab660965c02e4a248fb80e46d29109e
Raiz4G <91905270+Raiz4G@users.noreply.github.com>
2021-10-04 19:32:19 +0200

    Update Form1.cs

--- Monday Report Act/Form1.cs
+++ Monday Report Act/Form1.cs
@@ -16,5 +16,23 @@ namespace Monday_Report_Act
         {
             InitializeComponent();
         }
+        private void btnReadFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog f=new OpenFileDialog();
+            if(f.ShowDialog()==DialogResult.OK)
+            {
+                listBox1.Items.Clear();
+                List<string> lines=new List<string>();
+                using(StreamReader r=new StreamReader(f.OpenFile()))
+                {   
+                    string line;
+                    while((line=r.ReadLine())!=null)
+                    {
+                        listBox1.Itmes.Add(line);
+                    }
+                }
+            }
+        }
+        
     }
 }
